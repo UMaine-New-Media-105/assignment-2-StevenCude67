65f1a6d20110fb34f6daca45141202499d0a4749
StevenCude67 <124187339+StevenCude67@users.noreply.github.com>
2023-02-24 12:12:53 -0500

    Fixed and Renamed questionOne.js
    fixed code so it was the correct assignment and renamed file

--- questionOne.js
+++ questionOne.js
@@ -1,32 +1,19 @@
-let circleMove = 40;
-let colorCurrent = 220;
-let colorChange = 20;
-let backgroundColor = 30;
-
 function setup() {
   createCanvas(400, 400);
 }
-
+//background and color
 function draw() {
   background(220);
   fill("blue");
-  stroke("black");
-  newRow();
-  newRow(0, 80);
-  newRow();
-  newRow();
-  newRow();
-  
-  letterSquare();
-}
-function newRow(y) {
-  rect(0, 0, 80);
-  rect(80, 0, 80);
-  rect(160, 0, 80);
-  rect(240, 0, 80);
-  rect(320, 0, 80);
-}
-function letterSquare() {
+
+  //places each row of blue squares
+  newRow(0);
+  newRow(80);
+  newRow(160);
+  newRow(240);
+  newRow(320);
+
+  // Draws Initial Squares
   fill("orange");
   rect(80, 0, 80);
   rect(160, 0, 80);
@@ -39,4 +26,15 @@ function letterSquare() {
   rect(240, 320, 80);
   rect(160, 320, 80);
   rect(80, 320, 80);
+}
+
+//creates a row of blue squares
+function newRow(rowY) {
+  push();
+  rect(0, rowY, 80);
+  rect(80, rowY, 80);
+  rect(160, rowY, 80);
+  rect(240, rowY, 80);
+  rect(320, rowY, 80);
+  pop();
 }
